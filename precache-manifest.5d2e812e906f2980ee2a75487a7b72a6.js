42c505d8b0876b9550151dfa05db39472806647f
hanamhn <hanamhndev@gmail.com>
2020-10-01 15:34:50 +0700

    Updates

---
+++ precache-manifest.5d2e812e906f2980ee2a75487a7b72a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0ceb8810f0ca247c24dc86dfe1eb46",
+    "url": "/hanam-teacher/index.html"
+  },
+  {
+    "revision": "d8e5aaeb3223bae9f955",
+    "url": "/hanam-teacher/static/css/main.3a22b66b.chunk.css"
+  },
+  {
+    "revision": "9802fa5c09a4d70ac214",
+    "url": "/hanam-teacher/static/js/2.16433333.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hanam-teacher/static/js/2.16433333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8e5aaeb3223bae9f955",
+    "url": "/hanam-teacher/static/js/main.1d3e9202.chunk.js"
+  },
+  {
+    "revision": "d1259169827ae0fbd162",
+    "url": "/hanam-teacher/static/js/runtime-main.7aee99b6.js"
+  }
+]);
